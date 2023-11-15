1dad0b47e4a25fa8a94a15c21a00167b4ab0401c
Alamgir2-2 <darkthehunter22@gmail.com>
2023-11-16 01:22:27 +0600

    Hall image Added

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include("./app/header.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seat Allocation System</title>
+    <title>Seat Management System</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <!-- <style>
         .card-img-top {
@@ -68,8 +68,6 @@ include("./app/header.php");
                     <img src="./images/hall1.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Mir Mosharraf Hossain Hall</h5>
-                        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p> -->
                     </div>
                 </div>
             </div>
@@ -77,9 +75,7 @@ include("./app/header.php");
                 <div class="card cards">
                     <img src="./images/hall2.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">A. F. M. Kamaluddin Hall</h5>
-                        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p> -->
+                        <h5 class="card-title">Pritilata Hall</h5>
                     </div>
                 </div>
             </div>
@@ -88,8 +84,6 @@ include("./app/header.php");
                     <img src="./images/hall3.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h6 class="card-title">Bangabandhu Sheikh Mujibur Rahman Hall</h6>
-                        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content.</p> -->
                     </div>
                 </div>
             </div>
@@ -97,29 +91,24 @@ include("./app/header.php");
                 <div class="card cards">
                     <img src="./images/hall4.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Shaheed Salam Barkat Hall</h5>
-                        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p> -->
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card cards">
-                    <img src="./images/hall2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Maulana Bhashani Hall</h5>
-                        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p> -->
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card cards">
-                    <img src="./images/hall3.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
                         <h5 class="card-title">Shahid Rafiq Jabbar Hall</h5>
-                        <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card cards">
+                    <img src="./images/hall8.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Bangamata Begum Fazilatunnessa Mujib Hall</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card cards">
+                    <img src="./images/hall7.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Al Beruni Hall</h5>
+
                     </div>
                 </div>
             </div>
